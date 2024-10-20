0cc725c5e93559638de16de48359ec26e3bd84e7
Nathan876 <sequins.trancheur0c@icloud.com>
2024-10-20 23:23:21 +0200

    Rendu final list.c

--- list.c
+++ list.c
@@ -1,6 +1,6 @@
 #include "snake.h"
 
-pos *create_list(int x, int y)
+pos *create_list(int x, int y) // La fonction permet de créer une liste de position
 {
     pos *list = malloc(sizeof(*list));
     list->next = NULL;
@@ -10,8 +10,23 @@ pos *create_list(int x, int y)
     return list;
 }
 
+int isWinning(char **list){ // La fonction permet de vérifier si le joueur a gagné
+    int isTrue = 0;
+    for (int i = 0; i < 21; i++)
+    {
+        for (int j = 0; j < 21; j++)
+        {
+            if (list[i][j] ==  ' ' || list[i][j] == '2'){ // Si il n'y a plus de case de libre et il n'y a pas d'aliment alors le joueur a gagné
+                isTrue = 1;
+                return isTrue;
+            }
+        }
+    }
+    return isTrue;
+}
 
-void add_to_list(pos **list, int x, int y)
+
+void add_to_list(pos **list, int x, int y) // La fonction ajoute à la fin de la liste une nouvelle position
 {
     pos *tmp = *list;
 
@@ -27,15 +42,21 @@ void add_to_list(pos **list, int x, int y)
     tmp->next = item;
 }
 
+int isCollectingSpecial(char **board, fruits *fruit){ // La fonction permet de savoir si le joueur a collecté le fruit spécial
+    if (board[fruit->pos_x][fruit->pos_y] == '3') {
+        return 1;
+    }
+    return 0;
+}
 
-int mouvement(char **board, char coup, pos *snake, pos *box) {
-    int prev_x = snake->pos_x;
+int mouvement(char **board, char coup, pos *snake, fruits *fruit) { // La fonction mouvement gère le déplacement du serpent
+    int prev_x = snake->pos_x; // Stocke les positions actuelles du serpent
     int prev_y = snake->pos_y;
 
-    int new_x = snake->pos_x;
+    int new_x = snake->pos_x; // Stocke les positions actuelles du serpent
     int new_y = snake->pos_y;
 
-    if (coup == 'w') {
+    if (coup == 'w') { // En fonction de la lettre passée en paramètre le code modifie le paramètre du sens
         new_x++;
     } else if (coup == 's') {
         new_x--;
@@ -45,12 +66,12 @@ int mouvement(char **board, char coup, pos *snake, pos *box) {
         new_y--;
     }
 
-    if (board[new_x][new_y] == '1' || board[new_x][new_y] == '0') {
-        printf("C'est perdu");
+    if (board[new_x][new_y] == '1' || board[new_x][new_y] == '0') { // Si le joueur touche un mur ou que le serpent se mord c'est perdu
+        printf("C'est perdu !\n");
         return 1;
     }
 
-    if (board[new_x][new_y] == ' ') {
+    if (board[new_x][new_y] == ' ') { // Si la nouvelle position est libre alors le serpent se déplace sans s'agrandir
         pos *current = snake;
 
         while (current != NULL) {
@@ -91,35 +112,27 @@ int mouvement(char **board, char coup, pos *snake, pos *box) {
             board[current->pos_x][current->pos_y] = '0';
             current = current->next;
         }
-    } else if (board[new_x][new_y] == '2') {
-        add_to_list(&snake, box->pos_x, box->pos_y);
+
+    } else if (board[new_x][new_y] == '2' || board[new_x][new_y] == '3') { // Si sur la nouvelle position se trouve un fruit alors le serpent le mange et s'agrandit
+        add_to_list(&snake, fruit->pos_x, fruit->pos_y);
 
         int new_fruit_x = rand() % 20;
         int new_fruit_y = rand() % 20;
-        while (board[new_fruit_x][new_fruit_y] != ' ')
+        while (board[new_fruit_x][new_fruit_y] != ' ') // Une nouvelle position aléatoire est définie pour le fruit
         {
             new_fruit_x = rand() % 20;
             new_fruit_y = rand() % 20;
         }
-        board[box->pos_x][box->pos_y] = ' ';
-        box->pos_x = new_fruit_x;
-        box->pos_y = new_fruit_y;
-        board[box->pos_x][box->pos_y] = '2';
+        board[fruit->pos_x][fruit->pos_y] = ' ';
+        fruit->pos_x = new_fruit_x;
+        fruit->pos_y = new_fruit_y;
+        board[fruit->pos_x][fruit->pos_y] = '2';
     }
     return 0;
 }
 
-void insert_after(pos **link, int x, int y)
-{
-    pos *tmp = *link;
-    pos *item = malloc(sizeof(*item));
-    item->next = tmp->next;
-    item->pos_x = x;
-    item->pos_y = y;
-    tmp->next = item;
-}
 
-void delete_list(pos **list)
+void delete_list(pos **list) // Permet d'effacer la mémoire allouer pour le plateau
 {
     pos *tmp = *list;
 
